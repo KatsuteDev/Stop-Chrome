8ddf2d210b8b847d0f26f0b37718919d1d88700e
Katsute <58778985+Katsute@users.noreply.github.com>
2022-09-27 21:36:15 -0400

    update dependencies

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ type process = {
 // ----- main ---------------
 
 const name: string = "Stop Chrome";
-const version: string = "2.0.4";
+const version: string = "2.0.5";
 
 const icon : string = path.join(__dirname, "../", "assets", "icon.png");
 const green: string = path.join(__dirname, "../", "assets", "state_green.png");

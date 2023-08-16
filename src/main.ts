4441a44e9f90f3729de9423d305b3a5f7e212d9f
Katsute <58778985+Katsute@users.noreply.github.com>
2023-08-16 17:29:16 +0000

    prepare for release

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ type process = {
 // ----- main ---------------
 
 const name: string = "Stop Chrome";
-const version: string = "2.0.12";
+const version: string = "2.0.13";
 
 const icon : string = path.join(__dirname, "../", "assets", "icon.png");
 const green: string = path.join(__dirname, "../", "assets", "state_green.png");
